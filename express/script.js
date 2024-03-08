3342a5e7b1732f34e597fa04c6fcea305d1a7818
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-08 16:40:14 +0530

    Add middleware and dynamic routing.

--- express/script.js
+++ express/script.js
@@ -1,8 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get("/hello", (req,res) => {
-    res.send("Hello world")
+// middleware
+app.use((req, res, next) => {
+  console.log("middlware is working");
+  next()
 });
 
-app.listen(3000)
+// whatever brower sends --> req
+// whatever server sends --> res
+
+// route
+app.get("/", (req, res) => {
+//   console.log(req);
+  res.send("Hello world ok");
+});
+
+app.get("/profile", (req, res) => {
+    res.send("From Profile");
+})
+
+// dynamic routing
+app.get("/profile/:username", (req, res) => {
+    res.send(`Hello from ${req.params.username}`)
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
