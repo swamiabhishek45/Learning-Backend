57ba480ffa740511b53eb83d1ad74ffd222a1d22
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-12 19:03:25 +0530

    organize folders

--- Backend/frontend/src/App.jsx
+++ Backend/frontend/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/jokes") // 
+      .then((response) => {
+        console.log(response);
+        setJokes(response.data);
+    })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
 
   return (
     
     <>
     <h1>Chai piyo chai</h1>
+    <p>JOKES: {jokes.length}</p>
+
+    {
+      jokes.map((joke) => {
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      })
+    }
     </>
   )
 }
